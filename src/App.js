b0225a7303401a4c00e905a1eb1e4cb6434e64ac
Wojciech Bubolka <bubolka@gmail.com>
2019-06-11 23:30:51 +0100

    remove not needed assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Logo from './assets/sw-logo.svg'
 function App() {
   return (
     <div className="App">
+
+      <img src={Logo} alt="SWAPI" />
+
       
     </div>
   );
